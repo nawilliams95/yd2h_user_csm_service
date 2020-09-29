b5c8a8265d61b3273ba53beb41127ec4c9a841fd
Nykarri <karri.bonez@gmail.com>
2020-09-29 07:16:49 -0400

    added dependency
    faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create ([
+    {title: Faker::Book.title, content: Faker::Lorem.paragraphs(number: 2, supplemental: true), author: 'cutiewho', private: false, tags: ['fun', 'test', 'life'], user_id: 1 },
+    {title: Faker::Book.title, content: Faker::Lorem.paragraphs(number: 3, supplemental: true), author: 'peepingTom', private: false, tags: ['test', 'life'], user_id: 2 },
+    {title: Faker::Book.title, content: Faker::Lorem.paragraphs(number: 1, supplemental: true), author: 'cutiewho', private: false, user_id: 1 },
+    {title: Faker::Book.title, content: Faker::Lorem.paragraphs(number: 2, supplemental: true), author: 'cutiewho', private: true, tags: ['test'], user_id: 1 },
+    {title: Faker::Book.title, content: Faker::Lorem.paragraphs(number: 4, supplemental: true), author: 'greenmachine', private: false, tags: ['life'], user_id: 3 }
+])
+put 'Seeded database'
